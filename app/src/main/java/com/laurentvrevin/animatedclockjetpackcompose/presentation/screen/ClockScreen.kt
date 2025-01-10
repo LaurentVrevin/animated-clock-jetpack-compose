1efb963cfd875572c058b5a6bcda9a21e6dda9b4
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2025-01-10 15:04:47 +0100

    updated colors, synchronization of time with hands moving

--- app/src/main/java/com/laurentvrevin/animatedclockjetpackcompose/presentation/screen/ClockScreen.kt
+++ app/src/main/java/com/laurentvrevin/animatedclockjetpackcompose/presentation/screen/ClockScreen.kt
@@ -1,20 +1,14 @@
 package com.laurentvrevin.animatedclockjetpackcompose.presentation.screen
 
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -23,112 +17,126 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 
 @Composable
-fun ClockScreen(modifier: Modifier = Modifier){
-    val infiniteTransition = rememberInfiniteTransition(label = "")
+fun ClockScreen(modifier: Modifier = Modifier) {
 
+    var totalMinutes by remember { mutableIntStateOf(0) }
 
-    val minHeightWidth: Dp = 80.dp
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(1000L)
+            totalMinutes++
+        }
+    }
 
+    // Calculate hours and minutes from total minutes
+    val hours = (totalMinutes / 60) % 12
+    val minutes = totalMinutes % 60
 
-    val minutesRotation by infiniteTransition.animateFloat(
-        initialValue = 0F,
-        targetValue = 1F,
-        animationSpec = infiniteRepeatable(
-            animation = tween(60000, easing = LinearEasing)
-        ), label = ""
-    )
+    // Calculate rotation angles for hands
+    val minuteRotation = (minutes / 60f) * 360f
+    val hourRotation = (hours / 12f) * 360f + (minuteRotation / 12f)
 
-
-    val hourRotation by infiniteTransition.animateFloat(
-        initialValue = 0F,
-        targetValue = 1F,
-        animationSpec = infiniteRepeatable(
-            animation = tween(86400000, easing = LinearEasing)
-        ), label = ""
-    )
-
-
-
-
-    val minutes = (minutesRotation * 60).toInt()
-    val hours = (hourRotation * 12).toInt()
-
-
-
-
-    Column (modifier = Modifier
-        .padding(16.dp)
-        .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "%02d:%02d".format(hours, minutes),
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                // Gradient background
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFFa2d2ff),
+                        Color(0xFFfdf0d5),
+                    )
+                )
+            )
+    ) {
+        Column(
             modifier = Modifier
                 .padding(16.dp)
-                .align(Alignment.CenterHorizontally),
-            color = Color.Black,
-            fontWeight = FontWeight.Bold,
-            style = MaterialTheme.typography.headlineMedium,
-        )
-
-
-
-
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Canvas(
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.padding(16.dp))
+            // Counter Text
+            Text(
+                text = "%02d:%02d".format(hours, minutes),
                 modifier = Modifier
-                    .size(300.dp)
-                    .border(2.dp, color = Color.Black, shape = CircleShape)
-                    .background(Color.White, shape = CircleShape)
+                    .padding(16.dp)
+                    .align(Alignment.CenterHorizontally),
+                color = Color(0xFFfdf0d5),
+                fontWeight = FontWeight.Bold,
+                fontSize = 64.sp
+            )
+
+
+            // Clock
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
             ) {
-                val middle = Offset(size.minDimension / 2, size.minDimension / 2)
-                withTransform(
-                    {
-                        rotate(360f * minutesRotation, middle)
-                    }, {
-                        drawLine(
-                            strokeWidth = 6.dp.toPx(),
-                            cap = StrokeCap.Round,
-                            color = Color.Red,
-                            start = middle,
-                            end = Offset(
-                                size.minDimension / 2,
-                                (minHeightWidth / 2 - 20.dp).toPx()
+                Canvas(
+                    modifier = Modifier
+                        .size(300.dp)
+                        .border(8.dp, color = Color(0xFFff8fab), shape = CircleShape)
+                        .background(Color(0xffffe5ec), shape = CircleShape)
+                ) {
+                    val middle = Offset(size.minDimension / 2, size.minDimension / 2)
+                    val radius = size.minDimension / 2
+
+                    // Minutes
+                    withTransform(
+                        {
+                            rotate(minuteRotation, middle)
+                        }, {
+                            drawLine(
+                                strokeWidth = 6.dp.toPx(),
+                                cap = StrokeCap.Round,
+                                color = Color(0xFFffafcc),
+                                start = middle,
+                                end = Offset(
+                                    x = middle.x,
+                                    y = middle.y - radius * 0.8f
+                                )
                             )
-                        )
-                    }
-                )
-                withTransform(
-                    {
-                        rotate(360f * hourRotation, middle)
-                    }, {
-                        drawLine(
-                            strokeWidth = 6.dp.toPx(),
-                            cap = StrokeCap.Round,
-                            color = Color.Black,
-                            start = middle,
-                            end = Offset(
-                                size.minDimension / 2,
-                                (minHeightWidth / 2 - 12.dp).toPx()
+                        }
+                    )
+
+                    // Hours
+                    withTransform(
+                        {
+                            rotate(hourRotation, middle)
+                        }, {
+                            drawLine(
+                                strokeWidth = 8.dp.toPx(),
+                                cap = StrokeCap.Round,
+                                color = Color(0xFFcdb4db),
+                                start = middle,
+                                end = Offset(
+                                    x = middle.x,
+                                    y = middle.y - radius * 0.65f
+                                )
                             )
-                        )
-                    }
-                )
+                        }
+                    )
+                }
             }
         }
     }
 }
+
 @Preview
 @Composable
-fun ClockScreenPreview(){
+fun ClockScreenPreview() {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
         ClockScreen()
-        }
+    }
 }
